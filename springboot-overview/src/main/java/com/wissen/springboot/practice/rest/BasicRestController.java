8ce797928b69ff0ef9457e113e82268d4a2f9dbd
Dhaval-Wissen-Git <dhaval.solanki@wissen.com>
2024-09-04 13:02:52 +0530

    Custom properties.

--- springboot-overview/src/main/java/com/wissen/springboot/practice/rest/BasicRestController.java
+++ springboot-overview/src/main/java/com/wissen/springboot/practice/rest/BasicRestController.java
@@ -1,6 +1,7 @@
 package com.wissen.springboot.practice.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BasicRestController {
 
+    @Value("${message.hello}")
+    private String hello;
+
+    @Value("${message.bye}")
+    private String bye;
+
     @GetMapping("/")
     public String HelloWorld() {
-        return "Hello World";
+        return hello;
     }
 
     @GetMapping("/quit")
     public String Quit() {
-        return "Have a nice day. Bye!";
+        return "Have a nice day." + bye;
     }
 }
