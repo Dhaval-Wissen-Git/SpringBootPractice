99416b996eaa5c21f618a2fb438177b1a6537332
Dhaval-Wissen-Git <dhaval.solanki@wissen.com>
2024-09-17 01:57:32 +0530

    Added Postgres properties. Pending: DB Connection Exception resolving.

--- springboot-hibernate-jpa/src/main/java/com/wissen/springboot/practice/PracticeApplication.java
+++ springboot-hibernate-jpa/src/main/java/com/wissen/springboot/practice/PracticeApplication.java
@@ -1,12 +1,21 @@
 package com.wissen.springboot.practice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "com.wissen.springboot")
 public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return args -> {
+			System.out.println("Hello World!! Inside Command Line Runner");
+		};
 	}
 }
